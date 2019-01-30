677dea4457928f65bf8eab2e23627721bbebd888
yihangsama <yihangsama@10.251.205.154>
2019-01-30 02:10:58 -0800

    "Update Button" added To do: fix bugs about passing student id into the update function in Sevlet

--- web-student-tracker/src/com/lu2code/web/jdbc/StudentDbUtil.java
+++ web-student-tracker/src/com/lu2code/web/jdbc/StudentDbUtil.java
@@ -1,12 +1,14 @@
 package com.lu2code.web.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
+
 
 public class StudentDbUtil {
 	
@@ -72,6 +74,114 @@ public class StudentDbUtil {
 			exc.printStackTrace();
 		}
 	}
+
+
+	public void addStudent(Student theStudent) throws Exception {
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			//get connection
+			myConn = dataSource.getConnection();
+			//create sql for insert
+			String sql ="insert into student "
+						+ "(first_name, last_name, email) "
+						+ "values (?, ?, ?)";
+			myStmt = myConn.prepareStatement(sql);
+ 			//set the param values for the student
+			myStmt.setString(1, theStudent.getFirstName());
+			myStmt.setString(2, theStudent.getLastName());
+			myStmt.setString(3, theStudent.getEmail());
+			//execute sql insert
+			myStmt.execute();
+		} finally {
+	
+			//clean up JDBC objects
+			close(myConn, myStmt, null);
+		}
+		
+		
+	}
+
+	public Student getStudent(String theStudentId) throws Exception {
+		// TODO Auto-generated method stub
+		Student theStudent = null;
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		ResultSet myRs = null;
+		int studentId;
+		
+		try {
+			//convert student id to int;
+			studentId = Integer.parseInt(theStudentId);
+			
+			//get connection to database;
+			myConn = dataSource.getConnection();
+			
+			//create sql to get selected student
+			String sql = "select * from student where id=?";
+			
+			//create prepared statement
+			myStmt = myConn.prepareStatement(sql);
+			
+			//set params
+			myStmt.setInt(1, studentId);
+			
+			//excute statement
+			myRs = myStmt.executeQuery();
+			
+			//retrieve data from result set row
+			if(myRs.next()){
+				String firstName = myRs.getString("first_name");
+				String lastName = myRs.getString("last_name");
+				String email = myRs.getString("email");
+				
+				//use information to construct a new student object
+				theStudent = new Student(firstName, lastName, email);
+			} else {
+				throw new Exception("Could not find student id: " + studentId);
+			}
+			
+			return theStudent;
+			
+		} finally {
+			//clean JDBC object;
+			close(myConn, myStmt, myRs);
+		}
+	}
+
+	public void updateStudent(Student theStudent) throws Exception{
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		try {
+			
+			//get db connection
+			myConn = dataSource.getConnection();
+			
+			//create sql update statement
+			String sql = "update student " 
+						+ "set first_name=?, last_name=?, email=? " 
+						+ "where id=?";
+			
+			//prepare statement
+			myStmt = myConn.prepareStatement(sql);
+			
+			//set param
+			myStmt.setString(1, theStudent.getFirstName());
+			myStmt.setString(2, theStudent.getLastName());
+			myStmt.setString(3, theStudent.getEmail());
+			myStmt.setInt(4, theStudent.getId());
+			//execute sql statement
+			myStmt.execute();
+			
+		} finally {
+			//clean JDBC object
+			close(myConn, myStmt, null);
+		}
+	}
+	
 
 	
 	
